e1af67a1467486473fdc817b5e7638eb626588af
Melone <paul.oppeneiger@student.htldornbirn.at>
2025-09-19 09:04:49 +0200

    Common finished

---
+++ Common/Node.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using System;
+
+namespace Common
+{
+    public class Node
+    {
+        public Person Data;
+        public Node Next;
+        public Node(Person data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ Common/Person.cs
@@ -0,0 +1,37 @@
+﻿﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Common
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        private string _geschlecht = string.Empty;
+        public string Name { get; set; }
+        public string Geschlecht
+        {
+            get => _geschlecht;
+            set
+            {
+                if (value != "männlich" && value != "weiblich" && value != "Männlich" && value != "Weiblich")
+                {
+                    throw new ArgumentException("Ungültiges Geschlecht eingegeben!");
+                }
+                _geschlecht = value;
+            }
+        }
+        public DateTime Geburtstag { get; set; }
+        public int Alter => DateTime.Today.Year - Geburtstag.Year;
+        public override string ToString()
+        {
+            return $"Name: {Name}, Alter: {Alter}, Geschlecht: {Geschlecht}";
+        }
+        public Person(DateTime geburtstag, string geschlecht, string name)
+        {
+            Name = name;
+            Geburtstag = geburtstag;
+            Geschlecht = geschlecht;
+        }
+    }
+}
