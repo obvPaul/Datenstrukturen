233038bd4b69ce9373b63400b6ca101c1a17153c
Melone <paul.oppeneiger@student.htldornbirn.at>
2025-09-19 08:22:04 +0200

    added basic structure of the project

---
+++ EinzelnVerlinkterListenTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace EinzelnVerlinkterListenTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
